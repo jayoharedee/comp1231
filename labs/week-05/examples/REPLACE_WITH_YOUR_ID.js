525d3810153b82f3b954e738b7588b64e14a3eec
jayoharedee <jmorenstein@georgebrown.ca>
2020-01-27 22:05:17 -0500

    updated with renamed files for test

--- labs/week-05/examples/REPLACE_WITH_YOUR_ID.js
+++ labs/week-05/examples/REPLACE_WITH_YOUR_ID.js
@@ -1,4 +1,4 @@
 // remember the developer tools? open the "console" tab and have a look
 // at what's there. the console.log you see below allows us to print info
 // out to the console
-console.log("it works!");
+console.log("it works! BUT MAKE SURE YOU RENAME THE FILE HERE AND UPDATE THE HTML SCRIPT TAG");
