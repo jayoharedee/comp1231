683fd1e2cdc111f3b963806441b7842f4cf76f85
Jordan Michael <jordanm@tuta.io>
2020-03-18 01:07:53 -0400

    async test for script load

--- labs/week-12/exercises/countCookies.js
+++ labs/week-12/exercises/countCookies.js
@@ -34,3 +34,49 @@ function findPersistantCookies(cookie) {
 
 // set a cookie, not in a function, that will be accessible across the entire domain using JavaScript.
 //console.log(document.cookie) // on completion
+
+
+
+
+
+/***********
+ * 
+ */
+// utility method used for example
+/**
+ * Sets a cookie by name, value and any associated options
+ *
+ * @param {*} name
+ * @param {*} value
+ * @param {*} [options={}]
+ */
+function setCookie(name, value, options = {}) {
+  options = {
+    path: '/',
+    // add other defaults here if necessary
+    ...options
+  }
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString()
+  }
+
+  let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value)
+
+  for (let optionKey in options) {
+    updatedCookie += '; ' + optionKey
+    let optionValue = options[optionKey]
+    if (optionValue !== true) {
+      updatedCookie += '=' + optionValue
+    }
+  }
+
+  document.cookie = updatedCookie
+}
+
+
+
+setCookie('user', 'John', { secure: true, 'max-age': 3600 })
+setCookie('points', 36, { secure: true, 'max-age': 3600 })
+setCookie('weapons', 'none', { secure: true, 'max-age': 3600 })
+setCookie('level', 13, { secure: true, 'max-age': 3600 })
